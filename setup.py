1f81070c5c0dc3fd3f366b4a0c1f06338f6632a9
Yusuf Simonson <simonson@gmail.com>
2013-05-22 13:04:23 -0400

    Removed long description

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@
 
 from distutils.core import setup
 
-with open('README.md') as doc:
-  long_description = doc.read()
-
 setup(name='optfunc-ysimonson',
       version='0.1',
       description='Generate commandline flags from function arguments.',
@@ -16,6 +13,5 @@ setup(name='optfunc-ysimonson',
                    'License :: OSI Approved :: BSD License',
                    'Programming Language :: Python :: 2 :: Only',
                    'Environment :: Console',
-                   'Development Status :: 5 - Production/Stable'],
-      long_description=long_description,
+                   'Development Status :: 4 - Beta'],
       py_modules=['optfunc'])
